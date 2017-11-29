034c2218d8b2a329aec802822ec6fefd2c82d96a
hklhai <hkhai@outlook.com>
2017-11-29 16:50:50 +0800

    HotWord

--- src/main/java/cn/edu/ncut/bigdata/streaming/WindowHotWord.java
+++ src/main/java/cn/edu/ncut/bigdata/streaming/WindowHotWord.java
@@ -1,6 +1,7 @@
 package cn.edu.ncut.bigdata.streaming;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
@@ -10,6 +11,8 @@ import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
 
+import java.util.List;
+
 /**
  * 基于滑动窗口的热点搜索词实时统计
  * Created by Ocean lin on 2017/11/28.
@@ -17,7 +20,7 @@ import scala.Tuple2;
 public class WindowHotWord {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("WindowHotWord").setMaster("local[2]");
-        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(6));
+        JavaStreamingContext jsc = new JavaStreamingContext(conf, Durations.seconds(5));
 
         // 搜索日志的格式
         // leo hello
@@ -53,7 +56,38 @@ public class WindowHotWord {
             // 也就是说，每隔10秒钟，将最近60秒的数据，作为一个窗口，进行内部的RDD的聚合，然后统一对一个RDD进行后续
         }, Durations.seconds(60), Durations.seconds(10));
 
-        // windowDStream
+        // 每隔10秒钟，出来，之前60秒的收集到的单词的统计次数
+        // 执行transform操作，因为，一个窗口，就是一个60秒钟的数据，会变成一个RDD，然后，对这一个RDD
+        // 根据每个搜索词出现的频率进行排序，然后获取排名前3的热点搜索词
+        JavaPairDStream<String, Integer> finalRDD = windowDStream.transformToPair(new Function<JavaPairRDD<String, Integer>, JavaPairRDD<String, Integer>>() {
+            @Override
+            public JavaPairRDD<String, Integer> call(JavaPairRDD<String, Integer> javaPairRDD) throws Exception {
+                JavaPairRDD<Integer, String> reversePair = javaPairRDD.mapToPair(new PairFunction<Tuple2<String, Integer>, Integer, String>() {
+                    @Override
+                    public Tuple2<Integer, String> call(Tuple2<String, Integer> tuple2) throws Exception {
+                        return new Tuple2<>(tuple2._2, tuple2._1);
+                    }
+                });
+                JavaPairRDD<Integer, String> sortRDD = reversePair.sortByKey(false);
+
+                List<Tuple2<String, Integer>> top3 = sortRDD.mapToPair(new PairFunction<Tuple2<Integer, String>, String, Integer>() {
+                    @Override
+                    public Tuple2<String, Integer> call(Tuple2<Integer, String> t) throws Exception {
+                        return new Tuple2<>(t._2, t._1);
+                    }
+                }).take(3);
+
+                for (Tuple2<String, Integer> e : top3) {
+                    System.out.println(e._1 + ":" + e._2);
+                }
+
+
+                return javaPairRDD;
+            }
+        });
+
+        // 只是为了触发job的执行，所以必须有output操作
+        finalRDD.print();
 
 
         jsc.start();
